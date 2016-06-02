58be8a8388121305f45f4d3e0addb3d8c74d427e
DavidAmanfu <david.amanfu@yale.edu>
2016-06-02 18:50:52 -0400

    Add files via upload

---
+++ servo_test_easy/servo_test_easy.ino
@@ -0,0 +1,11 @@
+#include <Servo.h> // Include servo library
+Servo servoRight; // Declare left servo
+void setup() // Built in initialization block
+{
+servoRight.attach(13); // Attach left signal to pin 13
+servoRight.writeMicroseconds(1500); // 1.3 ms full speed clockwise
+}
+void loop() // Main loop auto-repeats
+{ // Empty, nothing needs repeating
+}
+
